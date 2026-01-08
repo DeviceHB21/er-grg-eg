a4de57143134bcfe7203f5e8325c36bb4f8bc345
Device21 <Device198356@gmail.com>
2026-01-08 23:24:46 +0200

    Update ScootLibrary.lua

--- UI/ScootLibrary.lua
+++ UI/ScootLibrary.lua
@@ -5218,7 +5218,7 @@ local Library do
                 Parent = Items["Side"].Instance,
                 Name = "\0",
                 BackgroundTransparency = 1,
-                Position = UDim2New(0, 0, 0, 30),
+                Position = UDim2New(0, 0, 0, 10),
                 BorderColor3 = FromRGB(0, 0, 0),
                 Size = UDim2New(1, 0, 1, -135),
                 BorderSizePixel = 0,
