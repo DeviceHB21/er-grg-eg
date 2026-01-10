537360ec2a5c88b52712bf7b5e9a1c646faf8180
Device21 <Device198356@gmail.com>
2026-01-11 01:10:16 +0200

    Reduce padding in UIListLayout from 4 to 2

--- UI/ScootLibrary.lua
+++ UI/ScootLibrary.lua
@@ -5948,7 +5948,7 @@ local Library do
             Instances:Create("UIListLayout", {
                 Parent = Items["Content"].Instance,
                 Name = "\0",
-                Padding = UDimNew(0, 4),
+                Padding = UDimNew(0, 2),
                 SortOrder = Enum.SortOrder.LayoutOrder
             })
 
